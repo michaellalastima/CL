ee9352e631eb65b485a8ad84a449f922631d3033
michaellalastima <60122577+michaellalastima@users.noreply.github.com>
2020-10-03 21:22:19 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>CL</title>
+			<link rel="stylesheet" type="text/css" href="css/style.css"/>
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+		<body>
+			<div class="wrapper">
+			<div class="body1"></div>
+			<div id="background-wrap">
+    <div class="x1">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x2">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x3">
+        <div class="cloud"></div>
+    </div>
+</div>
+</div>
+		<div class="square"></div>
+		<div class="oval1"></div>
+		<div class="oval2"></div>
+
+</div></div>
+</div>		
+			<div class="box"></div>
+						 <div class="element">
+		<center><br><br><a div class="fa fa-facebook">
+						<a div class="fa fa-twitter">
+						<a div class="fa fa-instagram">
+</body>
+</html>
+
+	
